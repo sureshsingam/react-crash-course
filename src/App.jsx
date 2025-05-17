dc17337a46b0045e07205965f2c9b1a76c24f3cd
Suresh <sureshsingam@gmail.com>
2025-05-16 22:56:13 -0400

    Added New post form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 
 import './App.css'
 import './components/PostsList'
+import { useState } from 'react';
+import MainHeader from "./components/MainHeader";
 import PostsList from './components/PostsList'
 
 function App() {
+  const [ modalVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler(){
+      setModalIsVisible(true);
+  }
+
+  function hideModalHandler(){
+      setModalIsVisible(false);
+  }
 
 
   return (
-    <main>
-      <PostsList />
-
-    </main>
-  
+    <>
+      <MainHeader onCreatePost={showModalHandler}/>
+      <main>
+        <PostsList onStopPosting={hideModalHandler} isPosting={modalVisible}/>
+      </main>
+    </>
   )
 }
 
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
+
 import Post from "./Post";
+import Modal from "./Modal";
+import NewPost from "./NewPost";
+
 import classes from './PostsList.module.css';
 
-function PostsList(){
 
+function PostsList( {onStopPosting, isPosting}){
+    const [posts, setPosts]= useState([]);
+
+    function addPostHandler(postData){
+        /*
+        setPosts([postData, ...posts]);  // use spread operator to add new post to the beginning of the array
+        // not optimal but works
+        */
+
+        setPosts((existingPosts) =>  [postData, ...existingPosts]); 
+        // use function to get the previous state and add new post to the beginning of the array
+    }
+    
     return(
-        <ul className={classes.posts}>
-            <Post author="Suresh" text="This is an example of prop 1"/>
-            <Post author="Ramesh" text="This is an example of prop 2"/>
-            <Post author="Mahesh" text="This is an example of prop 3"/>
-        </ul>
+        <>
+            {isPosting ? (
+                <Modal onClose={onStopPosting}>
+                    <NewPost onCancel={onStopPosting} onAddPost={addPostHandler}/>                
+                </Modal>
+            ) : null}
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map( (post)=>
+                        <Post key={post.body} author={post.author} text={post.body}/>
+                    )}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div style={{textAlign:'center', color:'white'}}>
+                    <h2>  There are no posts yet. </h2> 
+                    <p> Start adding some </p>
+                </div>
+
+            )}
+        </>
 
     );
 
